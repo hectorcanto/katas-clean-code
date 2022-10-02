22b147a5c50315b036f3d59bf6fbb6c006f3dad6
Hector Canto <hector.canto@citnow.com>
2022-10-02 22:03:59 +0200

    some renaiming, fix proxy list

--- katas/gilded_rose_proxy.py
+++ katas/gilded_rose_proxy.py
@@ -5,44 +5,44 @@ from .gilded_rose import Item, dispatch_updater
 
 class ItemProxy:
     def __init__(self, item: Item):
-        self._item = item
+        self.original_item = item
 
     @property
     def name(self):
-        return self._item.name
+        return self.original_item.name
 
     @property
     def sell_in(self):
-        return self._item.sell_in
+        return self.original_item.sell_in
 
     @sell_in.setter
     def sell_in(self, value):
-        self._item.sell_in = value
+        self.original_item.sell_in = value
 
     @property
     def quality(self):
-        return self._item.quality
+        return self.original_item.quality
 
     @quality.setter
     def quality(self, value):
-        self._item.quality = value
+        self.original_item.quality = value
 
     def update_quality(self):
         updater = dispatch_updater(self.name)
-        self.quality = updater(self._item)
+        self.quality = updater(self.original_item)
 
     def __repr__(self):
-        return self._item.__repr__()
+        return self.original_item.__repr__()
 
 
 class ProxyGildedRose:
     def __init__(self, items: List[Item]):
-        self._proxy = [ItemProxy(item) for item in items]
+        self.proxies = [ItemProxy(item) for item in items]
 
     @property
     def items(self):
-        return [proxy._item for proxy in self._proxies]
+        return [proxy.original_item for proxy in self.proxies]
 
     def update_quality(self):
-        for item in self.items:
+        for item in self.proxies:
             item.update_quality()
